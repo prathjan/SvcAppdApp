158270c5d6f91269a715b26007e48961466e3ceb
prathjan <prathjan@cisco.com>
2021-09-10 12:09:55 -0700

    org

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "global" {
   backend = "remote"
   config = {
-    organization = "Lab14"
+    organization = var.org
     workspaces = {
       name = var.globalwsname
     }
@@ -13,7 +13,7 @@ data "terraform_remote_state" "global" {
 data "terraform_remote_state" "dbvm" {
   backend = "remote"
   config = {
-    organization = "Lab14"
+    organization = var.org
     workspaces = {
       name = var.dbvmwsname
     }
@@ -23,13 +23,16 @@ data "terraform_remote_state" "dbvm" {
 data "terraform_remote_state" "appvm" {
   backend = "remote"
   config = {
-    organization = "Lab14"
+    organization = var.org
     workspaces = {
       name = var.appvmwsname
     }
   }
 }
 
+variable "org" {
+  type = string
+}
 variable "dbvmwsname" {
   type = string
 }
